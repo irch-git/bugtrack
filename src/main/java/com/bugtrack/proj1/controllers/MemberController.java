8e8da9de8c6cdf93f5a103d3299cc1fb5466799a
irch-git <irch.git@gmail.com>
2022-09-16 16:42:03 -0400

    set up attributes and objects for employee thymeleaf

--- src/main/java/com/bugtrack/proj1/controllers/MemberController.java
+++ src/main/java/com/bugtrack/proj1/controllers/MemberController.java
@@ -1,14 +1,23 @@
 package com.bugtrack.proj1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bugtrack.proj1.entities.Member;
 
 @Controller
 @RequestMapping("/members")
 public class MemberController {
 	
-	@RequestMapping("new")
-	public String displayMember() {
+	@RequestMapping(value = "new", method = RequestMethod.GET)
+	public String displayMember(Model model) {
+		
+		Member aMember = new Member();
+		
+		model.addAttribute("member", aMember);
+		
 		return "/member/new-member";
 	}
 }
