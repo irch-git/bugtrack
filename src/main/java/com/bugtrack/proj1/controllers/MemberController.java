4b8c2a2ad7b351fba2f3e862014d68f234c7b24f
irch-git <irch.git@gmail.com>
2022-09-18 14:08:38 -0400

    configured application properties for hibernate and thymeleaf

--- src/main/java/com/bugtrack/proj1/controllers/MemberController.java
+++ src/main/java/com/bugtrack/proj1/controllers/MemberController.java
@@ -2,22 +2,32 @@ package com.bugtrack.proj1.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bugtrack.proj1.entities.Member;
 
 @Controller
 @RequestMapping("/members")
 public class MemberController {
-	
-	@RequestMapping(value = "/new", method = RequestMethod.GET)
+
+//https://www.thymeleaf.org/doc/articles/springmvcaccessdata.html
+//form posts	
+	@GetMapping("/new")
 	public String displayMember(Model model) {
 		
 		Member aMember = new Member();
 		
 		model.addAttribute("member", aMember);
 		
-		return "/member/new-member";
+		return "member/new-member";
 	}
+	
+	@PostMapping("/saved")
+	public String saveMember(Model model, Member member) {
+		
+		return "saved";
+	}
+	
 }
